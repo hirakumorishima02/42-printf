1f323427d0f50e2a190bc15849e73241bc48b69c
Hiraku Morishima <hmorishi@c1r9s1.42tokyo.jp>
2021-05-19 14:25:25 +0900

    first commit

---
+++ header.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmorishi <hmorishi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/19 14:24:37 by hmorishi          #+#    #+#             */
+/*   Updated: 2021/05/19 14:25:02 by hmorishi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_H
+# define HEADER_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+#endif
